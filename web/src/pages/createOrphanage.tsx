45f03a033585e82ab3d52a2632f2305b54387fac
gustavo.tartare <gustavo.tartare@unibave.net>
2020-11-19 10:58:36 -0300

    Criado a página de Criação de Orfanatos

--- web/src/pages/createOrphanage.tsx
+++ web/src/pages/createOrphanage.tsx
@@ -1,38 +1,206 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { Map, Marker, TileLayer } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
+import { useHistory } from 'react-router-dom';
+
+import { FiPlus } from 'react-icons/fi';
 
 import Sidebar from '../components/Sidebar';
 import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
 
 import '../styles/pages/create-orphanages.css';
 
 const CreateOrpganage: React.FC = () => {
+    const history = useHistory();
+
+
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0 ,0]);
+    const [position, setPosition] = useState({ latitude: 0, longitude: 0})
+
+    const [name, setName] = useState('');
+    const [about, setAbout] = useState('');
+    const [instructions, setInstructions] = useState('');
+    const [opening_hours, setOpeningHours] = useState('');
+    const [open_on_weekends, setOpenOnWeekends] = useState(true);
+    const [images, setImages] = useState<File[]>([]);
+    const [previewImages, setPreviewImages] = useState<string[]>([]);
+
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+
+            setInitialPosition([latitude, longitude]);
+        })
+    }, []);
+
+    // Funções
+    function handleMapClick(event: LeafletMouseEvent){
+        const { lat, lng } = event.latlng;
+
+        
+        setPosition({
+            latitude: lat,
+            longitude: lng,
+        });
+    }
+
+    function handleSelectImages(event: ChangeEvent<HTMLInputElement>){
+        if(!event.target.files){
+            return;
+        }
+
+        const seletedImages = Array.from(event.target.files);
+
+        setImages(seletedImages);
+
+        const selectedImagesPreview = seletedImages.map(image => {
+            return URL.createObjectURL(image);
+        });
+
+        setPreviewImages(selectedImagesPreview);
+    }
+
+    function handleSubmit(event: FormEvent){
+        event.preventDefault();
+
+        const {latitude, longitude} = position;
+
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('about', about);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('instructions', instructions);
+        data.append('opening_hours', opening_hours);
+        data.append('open_on_weekends', String(open_on_weekends));
+        
+        images.forEach(image => {
+            data.append('images', image);
+        })
+
+        api.post('orphanages', data);
+
+        alert('Cadastro criado com sucesso')
+
+        history.push('/app');
+    }
+
   return (
       <div id="page-create-orphanage">
           <Sidebar/>
 
           <main>
-              <form onSubmit={()=>{}} className="create-orphanage-form">
-                  <fieldset>
+              <form onSubmit={handleSubmit} className="create-orphanage-form">
+                    <fieldset>
                       <legend>Dados</legend>
 
                       <Map
-                        center={[-28.3528488,-49.2909063]}
-                        style={{width: '100%', height: 100}}
-                        zoom={15}
+                        center={initialPosition}
+                        style={{width: '100%', height: 400}}
+                        zoom={14}
+                        onclick={handleMapClick}
                       >
                           
                         <TileLayer 
                             url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                         />
 
-                        <Marker
+                        { position.latitude !== 0 && (
+                            <Marker
                             interactive={false}
                             icon={mapIcon}
-                            position={[-28.3528488,-49.2909063]}
-                        />
+                            position={[
+                                position.latitude,
+                                position.latitude
+                            ]}
+                            />
+                        ) }
                       </Map>
-                  </fieldset>
+
+                      <div className="input-block">
+                          <label htmlFor="name">Nome</label>
+                          <input
+                            id="name"
+                            maxLength={300}
+                            value={name}
+                            onChange={event => setName(event.target.value)}
+                          />
+                      </div>
+
+                      <div className="input-block">
+                          <label htmlFor="name">Sobre <span>Máximo de 300 caracteres</span></label>
+                          <input
+                            id="name"
+                            maxLength={300}
+                            value={about}
+                            onChange={event => setAbout(event.target.value)}
+                          />
+                      </div>
+
+                      <div className="input-block">
+                          <label htmlFor="images">Fotos</label>
+
+                          <div className="images-container">
+                              { previewImages.map(image => {
+                                  return (
+                                    <img key={image}  src={image} alt={name}/>
+                                  )
+                              })}
+
+                              <label htmlFor="image[]" className="new-image">
+                                <FiPlus size={24} color="#15b6d6" />
+                              </label>
+                          </div>
+
+                          <input multiple onChange={handleSelectImages} type="file" id="image[]" />
+                      </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Visitação</legend>
+
+                        <div className="input-block">
+                            <label htmlFor="instructions">Instruções</label>
+                            <textarea 
+                                id="instructions"
+                                value={instructions}
+                                onChange={event => setInstructions(event.target.value)}
+                            />
+                        </div>
+
+                        <div className="input-block">
+                            <label htmlFor="open_on_weekends">Atende fim de semana</label>
+
+                            <div className="button-select">
+                                <button
+                                    type="button"
+                                    className={open_on_weekends ? 'active' : ''}
+                                    onClick={() => setOpenOnWeekends(true)}
+                                >
+                                    Sim
+                                </button>
+
+                                <button
+                                    type="button"
+                                    className={!open_on_weekends ? 'active' : ''}
+                                    onClick={() => setOpenOnWeekends(false)}
+                                >
+                                    Não
+                                </button>
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <button
+                        className="confirm-button"
+                        type="submit"
+                    >
+                        Confirmar
+                    </button>
+
               </form>
           </main>
       </div>
